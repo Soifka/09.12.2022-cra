d05c14c3851046c485fec3c7738ad52cbf7945da
Julia Alekseeva <soifka@gmail.com>
2022-12-11 21:16:15 +0200

    Lamp on/off

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false,
+    };
+  }
+
+  toggle() {
+    this.setState({
+      on: !this.state.on
+    })
+  }
+
   render() {
-    return <div className="circle"></div>;
+    const className = this.state.on ? "lamp-on" : "lamp-off";
+
+    return (
+      <React.Fragment>
+        <div className={className}></div>
+        <button onClick={() => {this.toggle()}}>On/Off</button>
+      </React.Fragment>
+    ) 
   }
 }
 
