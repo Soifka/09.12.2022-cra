b9f034ea612cb844be16f503b91b3b143b10d113
Julia Alekseeva <soifka@gmail.com>
2022-12-12 19:20:16 +0200

    Create function sortUsers

--- src/components/AlohaDashboard/AlohaDashboard.js
+++ src/components/AlohaDashboard/AlohaDashboard.js
@@ -1,38 +1,61 @@
 import React from "react";
 import Aloha from "../Aloha/Aloha";
 
-const usersArray = [
-    {
-        firstName: 'Tony',
-        lastName: 'Stark'
-    },
-    {
-        firstName: 'Bruce',
-        lastName: 'Brenner'
-    },
-    {
-        firstName: 'Thor',
-        lastName: 'Odinson'
-    },
-    {
-        firstName: 'Peter',
-        lastName: 'Parker'
-    },
-    {
-        firstName: 'Natasha',
-        lastName: 'Romanof'
-    }
-]
 
 class AlohaDashboard extends React.Component {
-  userToAloha() {
-    return usersArray.map((user) => <Aloha name={user.firstName} lastName={user.lastName} />)
+  constructor(props) {
+    super(props);
+    this.state = {
+        usersArray: [
+            {
+                firstName: 'Tony',
+                lastName: 'Stark'
+            },
+            {
+                firstName: 'Bruce',
+                lastName: 'Brenner'
+            },
+            {
+                firstName: 'Thor',
+                lastName: 'Odinson'
+            },
+            {
+                firstName: 'Peter',
+                lastName: 'Parker'
+            },
+            {
+                firstName: 'Natasha',
+                lastName: 'Romanof'
+            }
+        ]
+    }
   }  
+
+  userToAloha() {
+    const {usersArray} = this.state;
+    return usersArray.map((user) => <Aloha name={user.firstName} lastName={user.lastName} />)
+  }
+  
+  sortUsers() {
+    const {usersArray} = this.state;
+    const newArray = [...usersArray]; // создаем поверхностную копию массива usersArray
+    newArray.sort((a, b) => {
+        if(a.firstName > b.firstName) {
+            return 1;
+        }
+        return -1; 
+    });
+
+    this.setState({
+        usersArray: newArray
+    })
+  }
 
   render() {
     return (
       <>
         {this.userToAloha()}
+        <button onClick = {() => {this.sortUsers()}}>Sort</button>
       </>
     );
   }
