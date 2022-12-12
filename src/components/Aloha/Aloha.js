fa3ec673054e582d10dfc5cbc733e175b6d36913
Julia Alekseeva <soifka@gmail.com>
2022-12-12 18:25:39 +0200

    Rename Greeting to Aloha

--- src/components/Aloha/Aloha.js
+++ src/components/Aloha/Aloha.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class Greeting extends React.Component {
+class Aloha extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -17,14 +17,14 @@ class Greeting extends React.Component {
     render() {
         const whatSay = this.state.sayHello ? 'Hello' : 'Bye'; 
 
-        return <h1 onClick={() => {this.sayBye()}} >{whatSay}, {this.props.name} from {this.props.country}!</h1>
+        return <h1 onClick={() => {this.sayBye()}}>{whatSay}, {this.props.name} from {this.props.country}!</h1>
     }
 }
 
-export default Greeting;
+export default Aloha;
 
 /*
-Homework with Greeting
-Доробити компоненту Greetings таким чином, щоб за натиснення на елемент, текст "Hello ...." змінювався на "Bye!"
+Homework with Greeting/Aloha
+Доробити компоненту Greetings/Aloha таким чином, щоб за натиснення на елемент, текст "Hello ...." змінювався на "Bye!"
 Підказка. Працюйте зі стейтом.
 */
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import App from './components/App/App';
-import Greeting from './components/Greeting/Greeting';
+import App from './components/App/App';
+import Aloha from './components/Aloha/Aloha';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-      {/* <App /> */}
-      <Greeting name="Alex" country="USA" />
-      <Greeting name="Julia" country="Australia" />
-      <Greeting name="Masha" country="Ukraine" />
+      <App />
+      <Aloha name="Alex" country="USA" />
+      <Aloha name="Julia" country="Australia" />
+      <Aloha name="Masha" country="Ukraine" />
   </>
 );
