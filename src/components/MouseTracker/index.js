5f7e9e77017fa99d6dec704a0a6c942c13edd6ef
Julia Alekseeva <soifka@gmail.com>
2022-12-28 20:19:34 +0200

    SetState callback

--- src/components/MouseTracker/index.js
+++ src/components/MouseTracker/index.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 const MouseTracker = (props) => {
     const [coordinates, setCoordinates] = useState({
         x: 0,
-        y: 0
+        y: 0,
+        counter: 0
     })
 
     useEffect(() => {
@@ -14,16 +15,29 @@ const MouseTracker = (props) => {
     }, []);
 
     const tracker = (event) => {
-        setCoordinates({
-            x: event.clientX,
-            y: event.clientY
+        setCoordinates((prevState) => {
+            return {
+                ...prevState,
+                x: event.clientX,
+                y: event.clientY
+            }
+        })
+    }
+
+    const clickHandler = () => {
+        setCoordinates((prevState) => {
+            return {
+                ...prevState,
+                counter: prevState.counter + 1
+            }
         })
     }
 
     return (
-        <div>
+        <div onClick={clickHandler}>
             <p>X: {coordinates.x}</p>
             <p>Y: {coordinates.y}</p>
+            <p>Counter: {coordinates.counter}</p>
         </div>
     );
 }
