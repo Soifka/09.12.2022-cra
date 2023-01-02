838bd59807c6264a0a2cb2a8e3c41661897b16fd
Julia Alekseeva <soifka@gmail.com>
2023-01-02 22:13:15 +0200

    Formik validation with Yup + ErrorMessage

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import styles from './style.module.scss'
 import { SCHEMA } from '../../schemas';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 // isValid (работает асинхронно, возвращает promise)
 // isValidSync (работает синхронно, возвращает true/false)
@@ -20,15 +21,23 @@ const SignUpForm = (props) => {
     }
     
     return (
-        <Formik initialValues={initialState} onSubmit={handleSubmitToFormik}>
+        <Formik 
+        initialValues={initialState} 
+        onSubmit={handleSubmitToFormik}
+        validationSchema={SCHEMA}
+        >
             {
                 (formikProps) => {
                     return (
-                        <Form>
+                        <Form className={styles.formStyle}>
                             <Field name='name' placeholder='Your name' />
+                            <ErrorMessage name='name' component="p" />
                             <Field name='surname' placeholder='Your surname' />
+                            <ErrorMessage name='surname' component="p" />
                             <Field name='email' placeholder='Your email' />
+                            <ErrorMessage name='email' component="p" />
                             <Field name='pass' placeholder='Your password' />
+                            <ErrorMessage name='pass' component="p" />
                             <input type='submit' value='submit' />
                         </Form>
                     );
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,6 +3,6 @@ import * as yup from 'yup';
 export const SCHEMA = yup.object({
     name: yup.string().min(1).max(20),
     surname: yup.string().min(3).max(30),
-    email: yup.string().required().email(),
-    pass: yup.string().required().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
+    email: yup.string().required('Email required!!!').email(`Email isn't valid`),
+    pass: yup.string().required('Password required!!!').matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, 'Password is not valid!')
 })
