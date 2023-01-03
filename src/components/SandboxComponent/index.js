26fadfa84a01d8a39d190b359f189855cb226af0
Julia Alekseeva <soifka@gmail.com>
2023-01-03 20:39:47 +0200

    useCallback practice

--- src/components/SandboxComponent/index.js
+++ src/components/SandboxComponent/index.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from 'react';
 
 const Clicker = () => {
     const [click, setClick] = useState(0);
+    const [value, setValue] = useState('');
 
     // const clickHandler = () => {
     //     setClick(click + 1);
@@ -9,12 +10,27 @@ const Clicker = () => {
 
     const handler = useCallback(() => {
         setClick(click => click + 1);
-    }, [])
+    }, []);
+
+    const changeHandler = ({target: {value}}) => {
+        setValue(value);
+    }
+
+    const logValue = useCallback(() => {
+        let sum = 0;
+        for (let i = 0; i < 400000000; i++) {
+            sum += i;
+        }
+
+        console.log(value)
+    }, [value]);
 
     return (
         <>
             <h1>{click}</h1>
             <button onClick={handler}>Click</button>
+            <input type='text' value={value} onChange={changeHandler} />
+            <button onClick={logValue}>Click to log input value</button>
         </>
     );
 }
