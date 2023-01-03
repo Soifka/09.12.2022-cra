fc3f5ef2a01f748d5644f361b54f83b19f06c327
Julia Alekseeva <soifka@gmail.com>
2023-01-03 18:36:53 +0200

    useContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,53 @@
+
+/*
 import React, { Component } from 'react'
-import SignUpForm from './SignUpForm';
+import TreeWithTheme from './Tree';
+
 
 
 class App extends Component {
   
   render() {
     return (
-      <SignUpForm />
+      <TreeWithTheme />
     );
   }
 }
+*/
 
+
+//export default App;
+
+import React from "react";
+import Tree from './Tree';
+import { ThemeContext } from "../contexts/themeContext";
+import CONSTANTS from "../constants";
+import { UserContext } from "../contexts/userContext";
+const {THEMES} = CONSTANTS;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: THEMES.DARK,
+      user: {
+        firstName: 'John',
+        lastName: 'Doe 23'
+      }
+    }
+  }
+
+  render() {
+    const {user, theme} = this.state;
+    return (
+      <UserContext.Provider value={{user}}>
+        <ThemeContext.Provider value={[theme, () => {}]}>
+          <Tree />
+        </ThemeContext.Provider>
+      </UserContext.Provider>
+    );
+  }
+}
 
 export default App;
--- src/components/Tree/Parent/Subparent/Child/Innerchild/index.js
+++ src/components/Tree/Parent/Subparent/Child/Innerchild/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { withUser } from '../../../../../../HOC';
+import React, { useContext } from 'react';
+//import { withUser } from '../../../../../../HOC';
+import { UserContext } from '../../../../../../contexts/userContext';
 
 const Innerchild = (props) => {
+    const {user, logOut} = useContext(UserContext);
     return (
         <div style={{border: '3px solid black', padding:'25px'}}>
             <p>Innerchild</p>
-            <p>{JSON.stringify(props.user)}</p>
-            <button onClick={props.logOut}>Logout</button>
+            <p>{JSON.stringify(user)}</p>
+            <button onClick={logOut}>Logout</button>
         </div>        
     )       
 }
 
-const InnerchildWithUserContext = (props) => {
-    const InnerchildWithUserContext = withUser(Innerchild);
-    return <InnerchildWithUserContext />
-}
+// const InnerchildWithUserContext = (props) => {
+//     const InnerchildWithUserContext = withUser(Innerchild);
+//     return <InnerchildWithUserContext />
+// }
 
-export default InnerchildWithUserContext;
+//export default InnerchildWithUserContext;
+
+export default Innerchild;
