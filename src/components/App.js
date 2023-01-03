8bf6ee52608310b1fb8a56f6067eca6c4ea0f966
Julia Alekseeva <soifka@gmail.com>
2023-01-03 20:08:34 +0200

    Counter (date-fns)

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,20 @@
-
-/*
 import React, { Component } from 'react'
-import TreeWithTheme from './Tree';
-
+import Counter from './Counter/Counter';
+//import CounterPage from './Counter/CounterPage';
 
 
 class App extends Component {
   
   render() {
     return (
-      <TreeWithTheme />
+      <>
+        {/* <CounterPage /> */}
+        <Counter  />
+      </>
     );
   }
 }
-*/
 
-
-//export default App;
-
-import React from "react";
-import Tree from './Tree';
-import { ThemeContext } from "../contexts/themeContext";
-import CONSTANTS from "../constants";
-import { UserContext } from "../contexts/userContext";
-const {THEMES} = CONSTANTS;
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: THEMES.LIGHT,
-      user: {
-        firstName: 'John',
-        lastName: 'Doe 23'
-      }
-    }
-  }
-
-  setTheme = (theme) => {
-    this.setState({
-      theme
-    })
-  }
-
-  // logOut = () => {
-  //   this.setState({
-  //     user: {}
-  //   })
-  // }
-
-  render() {
-    const {user, theme} = this.state;
-    return (
-      <UserContext.Provider value={{user}}>
-        <ThemeContext.Provider value={[theme, this.setTheme]}>
-          <Tree />
-        </ThemeContext.Provider>
-      </UserContext.Provider>
-    );
-  }
-}
 
 export default App;
+
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, {  useState, useEffect } from "react";
+import { addSeconds, format } from 'date-fns';
 
+
+const Counter = (props) => {
+  const [time, setTime] = useState(new Date(0,0,0,0,0,0,0));
+  const [isRunning, setRunning] = useState(true);
+
+  let intervalId = null;
+
+  useEffect(() => {
+    if(isRunning) {
+      intervalId = setInterval(() => {
+        setTime(time => addSeconds(time, 1));
+      }, 1000);
+    }
+    
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning]);
+
+  const switchRunning = () => {
+    setRunning(!isRunning);
+  }
+  
+  return (
+    <>
+      <h1>
+        {format(time, 'HH:mm:ss')}
+      </h1>
+      <button onClick={switchRunning}>
+        {isRunning ? 'Stop' : 'Start'}
+      </button>
+    </>
+  );
+}
+
+export default Counter;
+
+/*
 class Counter extends React.Component {
   constructor(props) {
     super(props);
@@ -55,3 +94,4 @@ class Counter extends React.Component {
 }
 
 export default Counter;
+*/ 
